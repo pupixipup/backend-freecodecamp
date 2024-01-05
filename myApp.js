c49383600cb1d64dfaed93c46d88e976c6e4c7f1
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-05 22:11:51 +0100

    Update server

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 let express = require('express');
 let app = express();
 
+
 app.get("/", (req, res) => {
- res.send("Hello Express");
+ res.sendFile(__dirname + "/views/index.html")
 })
 
+app.get("/json", (req, res) => {
+ res.json({message: "Hello json"})
+})
 
-
-
+app.use("/public", express.static(__dirname + "/public"))
 
 
 
