0387488b0b169417572c8a6a9daf76a27ff35ea8
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-06 00:15:46 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,31 +2,32 @@ let express = require('express');
 require('dotenv').config();
 let app = express();
 
-app.use((req, res, next) => {
- console.log(req.method, req.path, "-", req.ip);
- next();
-})
+// app.use((req, res, next) => {
+//  console.log(req.method, req.path, "-", req.ip);
+//  next();
+// })
 
 app.use("/public", express.static(__dirname + "/public"))
 
-app.get("/now", (req, res, next) => {
- res.json({time: req.time});
-}, (req, res) => {
- req.time = new Date().toString()
-})
-
-app.get("/json", (req, res) => {
- let message = 'Hello json';
- if (process.env.MESSAGE_STYLE === "uppercase")
- {
-  message = message.toUpperCase();
- }
- res.json({ message })
-})
+// app.get("/now", (req, res, next) => {
+//  res.json({time: req.time});
+// }, (req, res) => {
+//  req.time = new Date().toString()
+// })
 
 
 app.get("/", (req, res) => {
  res.sendFile(__dirname + "/views/index.html")
+})
+
+
+
+app.get("/json", (req, res) => {
+ if (process.env.MESSAGE_STYLE === "uppercase")
+ {
+ res.json({"message": "HELLO JSON"})
+ }
+ res.json({"message": "Hello json"})
 })
 
 
