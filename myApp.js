b47eedc55ccf99c0e93206f7d03e2b630a85ccb0
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-06 00:39:44 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,18 +2,18 @@ let express = require('express');
 require('dotenv').config();
 let app = express();
 
-// app.use((req, res, next) => {
-//  console.log(req.method, req.path, "-", req.ip);
-//  next();
-// })
+app.use((req, res, next) => {
+ console.log(req.method, req.path, "-", req.ip);
+ next();
+})
 
 app.use("/public", express.static(__dirname + "/public"))
 
-// app.get("/now", (req, res, next) => {
-//  res.json({time: req.time});
-// }, (req, res) => {
-//  req.time = new Date().toString()
-// })
+app.get("/now", (req, res, next) => {
+ res.json({time: req.time});
+}, (req, res) => {
+ req.time = new Date().toString()
+})
 
 
 app.get("/", (req, res) => {
