6060bde6cfbb9c531ed83eb91a49784fa0b7014c
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-05 23:12:12 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,19 +1,30 @@
 let express = require('express');
+require('dotenv').config();
 let app = express();
+
+app.use((req, res, next) => {
+ console.log(req.method, req.path, "-", req.ip);
+ next();
+})
+
+app.use("/public", express.static(__dirname + "/public"))
+
+app.get("/now", (req, res, next) => {
+ next()
+ res.json({time: req.time});
+}, (req, res) => {
+ req.time = new Date().toString()
+})
+
+app.get("/json", (req, res) => {
+ const message = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
+ res.json({ message })
+})
 
 
 app.get("/", (req, res) => {
  res.sendFile(__dirname + "/views/index.html")
 })
-
-app.get("/json", (req, res) => {
- res.json({message: "Hello json"})
-})
-
-app.use("/public", express.static(__dirname + "/public"))
-
-
-
 
 
 
