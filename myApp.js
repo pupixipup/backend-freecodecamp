2cb5c23687f087aa8a60bae657807302c0af0e7c
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-09 22:13:32 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
+console.log("---- ENV ----");
+console.log("ENV: ", process.env.MONGO_URI);
+console.log(process.env);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let personSchema = new mongoose.Schema({
