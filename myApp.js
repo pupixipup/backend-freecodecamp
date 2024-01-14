5bb8b313b466e1a1430178df9c771dee8e86ee8b
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-01-14 15:06:11 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -109,7 +109,8 @@ const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person
       .find({food: foodToSearch})
-      .sort({name: -1}).limit(2)
+      .sort({name: -1})
+      .limit(2)
       .select({age: 0})
       .exec()
       .then((res) => {
